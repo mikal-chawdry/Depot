c574850045c72d37d4a0bf843f36d675026907e7
Mikal Chawdry <mikalchawdry@gmail.com>
2014-04-18 17:11:04 +0100

    Iteration E and F

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,9 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+  
   def index
     @products = Product.order(:title)
+    logger.debug("@cart gets set by the :set_cart before_action. The value of @cart is: #{@cart.inspect}")
   end
 end
